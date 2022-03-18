cb53abbc61ae7530cb79f3c09d1cc6aff9ce63c1
Syed Mohammad Ibrahim <iamibi@umd.edu>
2022-03-17 20:28:08 -0400

    Updated homework-4.py logic to not count duplicates

--- Homework/Homework - 4/homework-4.py
+++ Homework/Homework - 4/homework-4.py
@@ -23,39 +23,39 @@ def read_csv_file(filename: str):
         raise
 
 
-def write_csv_file(filename: str, field_names: tuple, data: list):
+def write_csv_file(filename: str, field_names: tuple, data: dict):
     try:
-        with open(filename, mode="w") as csv_file:
+        with open(filename, mode="w", newline="", encoding="utf-8") as csv_file:
             csv_writer = csv.DictWriter(csv_file, fieldnames=field_names)
 
             # Write headers
             csv_writer.writeheader()
 
             # Write each entry in the data
-            for row in data:
-                csv_writer.writerow(row)
-
+            for key in data.keys():
+                csv_writer.writerow(data[key])
     except Exception as ex:
         print(f"Something went wrong while trying to write to {filename}", str(ex))
         raise
 
 
 def get_remaining(ad_list: list, edr_list: list):
-    # Collect the hostnames list to and edr
+    # Collect the hostnames list of edr
     hostnames_edr = [edr["Hostname"] for edr in edr_list]
 
-    # Calculate the data on the systems remaining
-    edr_enrolled = 0
-    remaining = 0
-    list_remaining = []
+    # Collect the data on remaining hostname's
+    # Only count a hostname once.
+    hostnames_ad = {}
+    edr_enrolled_count = 0
     for ad in ad_list:
         if ad["Hostname"] in hostnames_edr:
-            edr_enrolled += 1
-        else:
-            remaining += 1
-            list_remaining.append(ad)
+            edr_enrolled_count += 1
+        elif ad["Hostname"] not in hostnames_ad:
+            hostnames_ad[ad["Hostname"]] = ad
 
-    return (list_remaining, edr_enrolled, remaining)
+    # Return the hostnames_ad dictionary which contains the remaining
+    # hostnames that are yet to enroll, already enrolled hostnames count
+    return hostnames_ad, edr_enrolled_count
 
 
 # Filenames
@@ -68,7 +68,7 @@ edr_csv_list = read_csv_file(edr_csv_file)
 ad_csv_list = read_csv_file(ad_csv_file)
 
 # Get the remaining data
-remaining_data, edr_enrolled, remaining_count = get_remaining(ad_csv_list, edr_csv_list)
+remaining_data, edr_enrolled = get_remaining(ad_csv_list, edr_csv_list)
 
 # Header fields
 fields = ("Hostname", "Operating System", "Build")
@@ -79,4 +79,4 @@ write_csv_file(remaining_csv_file, fields, remaining_data)
 # Print the output for verification
 print("Output of remaining systems to install EDR on written to remaining.csv\n")
 print(f"Systems enrolled in EDR: {edr_enrolled}")
-print(f"Systems needing to be enrolled in EDR: {remaining_count}")
+print(f"Systems needing to be enrolled in EDR: {len(remaining_data)}")
